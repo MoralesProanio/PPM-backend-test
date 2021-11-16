d8422fce5ddbc93640deb6f9453275985d12393b
Michael Oswaldo Morales Proaño <54379483+MoralesMichael@users.noreply.github.com>
2021-11-16 01:57:06 -0500

    migrations: tickets and users migration

---
+++ tickets-backend/database/migrations/2021_11_16_051804_create_tickets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin_city')->nullable();
+            $table->string('destination_city')->nullable();
+            $table->timestamp('departure_date')->nullable();
+            //relationship
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
