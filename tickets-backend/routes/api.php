f1e11ec62abd768c58de2e041529a3a51886d745
Michael Oswaldo Morales Proaño <54379483+MoralesMichael@users.noreply.github.com>
2021-11-16 03:04:51 -0500

    api: get methods to get the entire collection and specific records

--- tickets-backend/routes/api.php
+++ tickets-backend/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/users", [App\Http\Controllers\UserController::class, 'index']);
+
+Route::get("/tickets", [App\Http\Controllers\TicketController::class, 'index']);
+
+Route::get("/user/{user}", [App\Http\Controllers\UserController::class, 'show']);
+
+Route::get("/ticket/{ticket}", [App\Http\Controllers\TicketController::class, 'show']);
+
+
